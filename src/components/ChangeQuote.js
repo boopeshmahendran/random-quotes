2e01d21a11a0af0986f0695f93b2d56324b14840
Boopesh <boopeshmahendran@gmail.com>
2017-01-17 02:16:30 +0530

    Added styling for ChangeQuote component

--- src/components/ChangeQuote.js
+++ src/components/ChangeQuote.js
@@ -2,8 +2,15 @@ import React, { Component } from 'react';
 
 class ChangeQuote extends Component {
     render() {
+        var style = {
+            padding: 10,
+            color: 'white',
+            fontSize: '1.1rem',
+            backgroundColor: '#D84315',
+            border: 'none'
+        };
         return (
-            <button onClick={this.props.changeQuote}> ChangeQuote</button>
+            <button style={style} onClick={this.props.changeQuote}>Change Quote</button>
         );
     }
 }
